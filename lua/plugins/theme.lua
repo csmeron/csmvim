75e4565cba511a0e745deefa95f6cb827deb1792
csmeron <csmeron@proton.me>
2025-07-31 13:44:45 -0500

    amending because forgot to add lazy-lock

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,20 @@
+ -- Spec for setting up colorscheme (Everforest)
+ 
+ return {
+     "neanias/everforest-nvim",
+     version = false,
+     lazy = false,
+     priority = 1000,
+     config = function()
+         require("everforest").setup({
+             background = "medium", -- "soft", "medium", "hard"
+             transparent_background_level = 0.1, -- 0 to 1
+             dim_inactive_windows = true,
+             styles = {
+                 comments = "italic",
+                 keywords = "bold",
+                 variables = "italic",
+             },
+         })
+     end,
+ }
