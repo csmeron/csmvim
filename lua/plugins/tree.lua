54a59fb5bf8fda0075b645ddc3a8794cdeee77af
csmeron <csmeron@proton.me>
2025-07-31 15:14:53 -0500

    treesitter, which-key, etc.

---
+++ lua/plugins/tree.lua
@@ -0,0 +1,81 @@
+-- treesitter and related plugins
+return {
+    -- which-key
+    {
+        "folke/which-key.nvim",
+        opts = {
+            spec = {
+                { "<BS>", desc = "Decrement Selection", mode = "x" },
+                { "<c-space>", desc = "Increment Selection", mode = { "x", "n" } },
+            },
+        },
+    },
+    -- treesitter
+    "nvim-treesitter/nvim-treesitter",
+    version = false,
+    build = ":TSUpdate",
+    event = { "LazyFile", "VeryLazy" },
+    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening from the cmdline
+    init = function(plugin)
+        require("lazy.core.loader").add_to_rtp(plugin)
+        require("nvim-treesitter.query_predicates")
+    end,
+    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+    keys = {
+        { "<c-space>", desc = "Increment Selection" },
+        { "<bs>", desc = "Decrement Selection", mode = "x" },
+    },
+    opts_extend = { "ensure_installed" },
+    ---@type TSConfig
+    ---@diagnostic disable-next-line: missing-fields
+    opts = {
+        highlight = { enable = true },
+        indent = { enable = true },
+        ensure_installed = {
+            "bash",
+            "c",
+            "html",
+            "javascript",
+            "typescript",
+            "json",
+            "java",
+            "lua",
+            "luadoc",
+            "luap",
+            "markdown",
+            "markdown_inline",
+            "python",
+            "vim",
+            "vimdoc",
+            "rust",
+            "xml",
+        },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "<C-space>",
+                node_incremental = "<C-space>",
+                scope_incremental = false,
+                node_decremental = "<bs>",
+            },
+        },
+        textobjects = {
+            move = {
+                enable = true,
+                goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+                goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+                goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+                goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+            },
+        },   
+    },
+    {
+        "windwp/nvim-ts-autotag",
+        opts = {},
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        event = "VeryLazy",
+        enabled = true,
+    },
+}
