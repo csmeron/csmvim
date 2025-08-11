b132a66c68d051355ed5120c543cec5744eecb30
csmeron <csmeron@proton.me>
2025-08-11 17:56:38 -0500

    implemented noice, mini.icons, and nui

--- lua/plugins/ui/ui.lua
+++ lua/plugins/ui/ui.lua
@@ -1,4 +1,4 @@
----@diagnostic disable: undefined-global
+---@diagnostic disable: undefined-global, different-requires
 local iconfig = require("core.icons")
 
 return {
@@ -65,4 +65,124 @@ return {
 		end,
 	},
 	-- lualine to replace status bar
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons", -- fore file icons
+			"Smiteshp/nvim-navic", -- for LSP symbols
+		},
+	},
+	-- noice to improve style of ui/notifications
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = require("config.noice"),
+		dependencies = {
+			"MuniTanjim/nui.nvim", -- for noice ui components
+			"rcarriga/nvim-notify", -- for notifications
+		},
+		keys = {
+			{ "<leader>sn", "", desc = "+noice" },
+			{
+				"<S-Enter>",
+				function()
+					require("noice").redirect(vim.fn.getcmdline())
+				end,
+				mode = "c",
+				desc = "Redirect Cmdline",
+			},
+			{
+				"<leader>snl",
+				function()
+					require("noice").cmd("last")
+				end,
+				desc = "Noice Last Message",
+			},
+			{
+				"<leader>snh",
+				function()
+					require("noice").cmd("history")
+				end,
+				desc = "Noice History",
+			},
+			{
+				"<leader>sna",
+				function()
+					require("noice").cmd("all")
+				end,
+				desc = "Noice All",
+			},
+			{
+				"<leader>snd",
+				function()
+					require("noice").cmd("dismiss")
+				end,
+				desc = "Dismiss All",
+			},
+			{
+				"<leader>snt",
+				function()
+					require("noice").cmd("pick")
+				end,
+				desc = "Noice Picker (Telescope/FzfLua)",
+			},
+			{
+				"<c-f>",
+				function()
+					if not require("noice.lsp").scroll(4) then
+						return "<c-f>"
+					end
+				end,
+				silent = true,
+				expr = true,
+				desc = "Scroll Forward",
+				mode = { "i", "n", "s" },
+			},
+			{
+				"<c-g>",
+				function()
+					if not require("noice.lsp").scroll(-4) then
+						return "<c-g>"
+					end
+				end,
+				silent = true,
+				expr = true,
+				desc = "Scroll Backward",
+				mode = { "i", "n", "s" },
+			},
+		},
+		config = function(_, opts)
+			-- HACK: noice shows messages from before it was enabled,
+			-- but this is not ideal when Lazy is installing plugins,
+			-- so clear the messages in this case.
+			if vim.o.filetype == "lazy" then
+				vim.cmd([[messages clear]])
+			end
+			require("noice").setup(opts)
+		end,
+	},
+	-- mini.icons
+	{
+		"echasnovski/mini.icons",
+		lazy = true,
+		opts = {
+			-- i love big toes, only the big toes. ~p4rke
+			file = {
+				[".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
+				["devcontainer.json"] = { glyph = "", hl = "MiniIconsAzure" },
+			},
+			filetype = {
+				dotenv = { glyph = "", hl = "MiniIconsYellow" },
+			},
+		},
+		init = function()
+			package.preload["nvim-web-devicons"] = function()
+				require("mini.icons").mock_nvim_web_devicons()
+				return package.loaded["nvim-web-devicons"]
+			end
+		end,
+	},
+
+	-- extra ui components
+	{ "MunifTanjim/nui.nvim", lazy = true },
 }
