6e55f953607c6ff4f9c00e1ccba9f1637f2ba90c
csmeron <csmeron@proton.me>
2025-08-11 17:26:38 -0500

    added two more generic mappings that don't yet fit a category

--- lua/core/keymaps/init.lua
+++ lua/core/keymaps/init.lua
@@ -8,5 +8,7 @@ vim.api.nvim_set_keymap("n", ";", ":", { noremap = true, silent = true })
 -- general save/copy
 util.map("n", "<C-s>", "<cmd>w<CR>", opts) -- general save file
 util.map("n", "<C-c>", "<cmd>%y+<CR>", opts) -- general copy (entire) file
+util.map("v", "<", "<gv", opts)
+util.map("v", ">", ">gv", opts)
 
 util.load_directory("core.keymaps", { exclude = { "init" } })
