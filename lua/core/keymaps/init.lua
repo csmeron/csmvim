a3ca81ea988a661c9c76b76df3442342285fad9a
csmeron <csmeron@proton.me>
2025-08-11 17:47:57 -0500

    added note about plugin spec mappings

--- lua/core/keymaps/init.lua
+++ lua/core/keymaps/init.lua
@@ -4,6 +4,9 @@ local util = require("core.util")
 vim.g.mapleader = " "
 vim.api.nvim_set_keymap("n", ";", ":", { noremap = true, silent = true })
 
+-- Some keymaps are defined in their respective plugins' specs, such as
+-- bufferline, for example, as its mappings are part of its setup.
+
 -- generic maps that don't fit any specific category yet
 -- general save/copy
 util.map("n", "<C-s>", "<cmd>w<CR>", opts) -- general save file
