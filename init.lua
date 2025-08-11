a24a6581f7bc25bde3eba4385f03523366bc462b
csmeron <csmeron@proton.me>
2025-08-11 16:31:33 -0500

    Redid init using tables so the core and plugin modules are easily viewable in init

--- init.lua
+++ init.lua
@@ -1,11 +1,17 @@
----@diagnostic disable: undefined-global
-vim.uv = vim.uv or vim.loop
+-- Bootstrap lazy.nvim and import modules
+-- Import plugins (these will be used in core.lazy)
+local plugs = {
+	{ import = "plugins.ui" },
+}
+-- Import core modules
+local core = {
+	"core.options",
+	"core.autocmds",
+}
 
-local M = {}
-
----@param opts? CoreConfig
-function M.setup(opts)
-	require("core").setup(opts)
+-- send plugins to the lazy bootstrap
+require("core.lazy").setup(plugs)
+-- require all core modules
+for _, mod in ipairs(core) do
+	require(mod)
 end
-
-return M
