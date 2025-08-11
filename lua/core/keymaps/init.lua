82089c75aea18312b660a56ac6a9cb276d76e2a4
csmeron <csmeron@proton.me>
2025-08-11 17:17:36 -0500

    added generic save mappings to keymaps.init

--- lua/core/keymaps/init.lua
+++ lua/core/keymaps/init.lua
@@ -4,4 +4,9 @@ local util = require("core.util")
 vim.g.mapleader = " "
 vim.api.nvim_set_keymap("n", ";", ":", { noremap = true, silent = true })
 
+-- generic maps that don't fit any specific category yet
+-- general save/copy
+util.map("n", "<C-s>", "<cmd>w<CR>", opts) -- general save file
+util.map("n", "<C-c>", "<cmd>%y+<CR>", opts) -- general copy (entire) file
+
 util.load_directory("core.keymaps", { exclude = { "init" } })
