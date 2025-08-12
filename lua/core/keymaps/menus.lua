8d8bda38a1ea8b77a319e99a7c7a02dd16cb331d
csmeron <csmeron@proton.me>
2025-08-12 08:35:10 -0500

    Added explorer keymaps

--- lua/core/keymaps/menus.lua
+++ lua/core/keymaps/menus.lua
@@ -4,6 +4,13 @@ local map = require("core.util").map
 
 -- Exit to netrw
 map("n", "<leader>pv", vim.cmd.Ex, opts, { desc = "Open netrw" })
+-- Open explorer (Snacks)
+map("n", "<leader>e", function()
+    require("snacks").explorer()
+end, { desc = "Toggle Explorer" })
+map("n", "<C-e>", function()
+    require("snacks").explorer()
+end, { desc = "Toggle Explorer" })
 
 -- Theme swapper
 map("n", "<leader>th", "<cmd>Themery<CR>", { desc = "Open theme switcher" })
