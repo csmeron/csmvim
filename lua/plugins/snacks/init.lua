0e18ed8550596a274c281a19e82206e175f21539
csmeron <csmeron@proton.me>
2025-08-12 08:29:37 -0500

    Added comment message about certain spec locations at the top of file

--- lua/plugins/snacks/init.lua
+++ lua/plugins/snacks/init.lua
@@ -1,4 +1,8 @@
 ---@diagnostic disable: undefined-global
+--- 
+--- Some snacks modules are defined in separate specs.
+--- For example, the explorer config is defined in plugins.editor.explore
+--- This is just for sorting purposes, and it may change down the line
 return {
 	"folke/snacks.nvim",
 	priority = 1000,
