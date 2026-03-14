d3ad9e50c4ae7beccc26d57781c356664a657f43
Cam <csmeron@proton.me>
2026-03-14 13:26:20 -0500

    Added other icon configs and theme options

--- lua/plugins/ui/ui.lua
+++ lua/plugins/ui/ui.lua
@@ -16,4 +16,27 @@ return {
 			"meuter/lualine-so-fancy.nvim",
 		},
 	},
+
+    -- mini.icons
+	{
+		"echasnovski/mini.icons",
+		lazy = true,
+		opts = {
+			file = {
+				[".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
+				["devcontainer.json"] = { glyph = "", hl = "MiniIconsAzure" },
+			},
+			filetype = {
+				dotenv = { glyph = "", hl = "MiniIconsYellow" },
+			},
+		},
+		init = function()
+			package.preload["nvim-web-devicons"] = function()
+				require("mini.icons").mock_nvim_web_devicons()
+				return package.loaded["nvim-web-devicons"]
+			end
+		end,
+	},
+    -- extra ui components
+	{ "MunifTanjim/nui.nvim", lazy = true },
 }
