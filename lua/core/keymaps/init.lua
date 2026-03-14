b43e282854f2176e5ce3048007af37821433f94e
Cam <csmeron@proton.me>
2026-03-14 14:06:11 -0500

    Start of keymap spec file, pushing for file swap

---
+++ lua/core/keymaps/init.lua
@@ -0,0 +1,3 @@
+---@diagnostic disable: undefined-global
+local util = require("core.util")
+-- binds `vim.keymap.set` to `util.map`
---
+++ lua/core/util.lua
@@ -0,0 +1,26 @@
+--@diagnostic disable: undefined-global
+
+-- This file contains any utility functions
+local M = {}
+
+-- Keymap helper
+M.map = vim.keymap.set
+
+function M.load_directory(module_prefix, opts)
+	local exclude = {}
+	for _, name in ipairs(opts.exclude or {}) do
+		exclude[name] = true
+	end
+
+	local path = vim.fn.stdpath("config") .. "/lua/" .. module_prefix:gsub("%.", "/")
+	for name, type in vim.fs.dir(path) do
+		if type == "file" and name:match("%.lua$") then
+			local base = name:gsub("%.lua$", "")
+			if not exclude[base] then
+				require(module_prefix .. "." .. base)
+			end
+		end
+	end
+end
+
+return M
