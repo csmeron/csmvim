94a042bbcb435da0329405cce911a9808eac84db
Cam <csmeron@proton.me>
2026-03-25 15:46:50 -0500

    Added in mini.pairs for auto-pairing during setup

---
+++ lua/plugins/coding/coding.lua
@@ -0,0 +1,23 @@
+-- Coding-related plugins such as pairs and snippets.
+return {
+    -- auto pairs currently done with mini.pairs
+    -- may be replaced in future with mini.nvim or nvim-autopairs
+    {
+        "echasnovski/mini.pairs",
+        events = "VeryLazy",
+        opts = {
+			modes = { insert = true, command = true, terminal = false },
+			-- skip autopair when next character is any of these
+			skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+			-- skip autopair when the cursor is inside these treesitter nodes
+			skip_ts = { "string" }, -- to make strings neater and not annoying as balls
+			-- skip autopair when next character is a closing pair and there are more
+			-- closing pairs than opening pairs
+			skip_unbalanced = true,
+			-- better deal with markdown code blocks
+			markdown = true,
+		},
+    }
+
+
+}
